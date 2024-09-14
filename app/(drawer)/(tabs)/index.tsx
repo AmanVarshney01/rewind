28c41b08fc672aceb790159a0dec546c379a391d
Aman Varshney <av.amanvarshney11@gmail.com>
2024-09-14 20:41:56 +0530

    cleanup

--- app/(drawer)/(tabs)/index.tsx
+++ app/(drawer)/(tabs)/index.tsx
@@ -1,14 +1,14 @@
 import { Stack } from 'expo-router';
+import { Text } from 'react-native';
 
 import { Container } from '~/components/Container';
-import { ScreenContent } from '~/components/ScreenContent';
 
 export default function Home() {
   return (
     <>
       <Stack.Screen options={{ title: 'Tab One' }} />
       <Container>
-        <ScreenContent path="app/(drawer)/(tabs)/index.tsx" title="Tab One" />
+        <Text>RDJWIN</Text>
       </Container>
     </>
   );
--- app/modal.tsx
+++ app/modal.tsx
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { Platform } from 'react-native';
-
-import { ScreenContent } from '~/components/ScreenContent';
 
 export default function Modal() {
   return (
     <>
-      <ScreenContent path="app/modal.tsx" title="Modal" />
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </>
   );
