9bc1a3ffce0d8badc14ca045a762c889122f993c
Aman Varshney <av.amanvarshney11@gmail.com>
2024-09-15 09:48:01 +0530

    switch to bun

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -1,5 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Pressable, View } from 'react-native';
+
 import { setAndroidNavigationBar } from '~/lib/android-navigation-bar';
 import { MoonStar } from '~/lib/icons/MoonStar';
 import { Sun } from '~/lib/icons/Sun';
@@ -16,19 +17,17 @@ export function ThemeToggle() {
         setAndroidNavigationBar(newTheme);
         AsyncStorage.setItem('theme', newTheme);
       }}
-      className='web:ring-offset-background web:transition-colors web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2'
-    >
+      className="web:ring-offset-background web:transition-colors web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2">
       {({ pressed }) => (
         <View
           className={cn(
-            'flex-1 aspect-square pt-0.5 justify-center items-start web:px-5',
+            'aspect-square flex-1 items-start justify-center pt-0.5 web:px-5',
             pressed && 'opacity-70'
-          )}
-        >
+          )}>
           {isDarkColorScheme ? (
-            <MoonStar className='text-foreground' size={23} strokeWidth={1.25} />
+            <MoonStar className="text-foreground" size={23} strokeWidth={1.25} />
           ) : (
-            <Sun className='text-foreground' size={24} strokeWidth={1.25} />
+            <Sun className="text-foreground" size={24} strokeWidth={1.25} />
           )}
         </View>
       )}
