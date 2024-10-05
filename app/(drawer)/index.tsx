3e11ea47d559cb121350dee710c5864df476f886
Aman Varshney <av.amanvarshney11@gmail.com>
2024-10-05 07:49:11 +0530

    update biome scripts

--- app/(drawer)/index.tsx
+++ app/(drawer)/index.tsx
@@ -1,15 +1,14 @@
-import { Stack } from 'expo-router'
-
 import { Container } from '@/components/Container'
 import { Button } from '@/components/ui/button'
 import { Text } from '@/components/ui/text'
+import { Stack } from 'expo-router'
 
 export default function Home() {
   return (
     <>
       <Stack.Screen options={{ title: 'Home' }} />
       <Container>
-        <Text className=" text-4xl">YOOsss</Text>
+        <Text className=" text-4xl">YOO</Text>
         <Button>
           <Text className="text-background">Click me</Text>
         </Button>
